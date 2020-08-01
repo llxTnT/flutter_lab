c8a16922dcf05ad8c40b62171db127727aee235f
llxTnT <13423743765@163.com>
2020-08-02 00:32:54 +0800

    添加显示Button 202008020030

--- lib/ui/sample/common_view_sample_page.dart
+++ lib/ui/sample/common_view_sample_page.dart
@@ -21,6 +21,10 @@ class CommonViewPage extends StatefulWidget {
 }
 
 class _CommonViewPageState extends State<CommonViewPage> {
+  Image assetsImg;
+
+  String  dropdownValue="drop item1";
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -30,102 +34,183 @@ class _CommonViewPageState extends State<CommonViewPage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.all(5),
-                  child: Text("Im Text View 1 NormalText"),
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.all(5),
-                  child: Text(
-                    "Im Text View 2 Big Text",
-                    style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: SingleChildScrollView(
+            child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.all(5),
+                    child: Text("Im Text View 1 NormalText"),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.all(5),
+                    child: Text(
+                      "Im Text View 2 Big Text",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.all(5),
+                    child: Text.rich(
+                      TextSpan(children: [
+                        TextSpan(
+                            text: 'Im ',
+                            style: TextStyle(
+                                color: Color.fromARGB(
+                                    255,
+                                    Random().nextInt(255),
+                                    Random().nextInt(255),
+                                    Random().nextInt(255)))),
+                        //第1个片段，红色样式
+                        TextSpan(
+                            text: 'Text ',
+                            style: TextStyle(
+                                color: Color.fromARGB(
+                                    255,
+                                    Random().nextInt(255),
+                                    Random().nextInt(255),
+                                    Random().nextInt(255)))),
+                        //第1个片段，黑色样式
+                        TextSpan(
+                            text: 'View 3 ',
+                            style: TextStyle(
+                                color: Color.fromARGB(
+                                    255,
+                                    Random().nextInt(255),
+                                    Random().nextInt(255),
+                                    Random().nextInt(255)))),
+                        //第1个片段，红色样式
+                        TextSpan(
+                            text: 'Color Text',
+                            style: TextStyle(
+                                color: Color.fromARGB(
+                                    255,
+                                    Random().nextInt(255),
+                                    Random().nextInt(255),
+                                    Random().nextInt(255))))
+                        //第1个片段，黑色样式
+                      ]),
+                      textAlign: TextAlign.center,
+                    ),
                   ),
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.all(5),
-                  child: Text.rich(
-                    TextSpan(children: [
-                      TextSpan(
-                          text: 'Im ',
-                          style: TextStyle(
-                              color: Color.fromARGB(
-                                  255,
-                                  Random().nextInt(255),
-                                  Random().nextInt(255),
-                                  Random().nextInt(255)))),
-                      //第1个片段，红色样式
-                      TextSpan(
-                          text: 'Text ',
-                          style: TextStyle(
-                              color: Color.fromARGB(
-                                  255,
-                                  Random().nextInt(255),
-                                  Random().nextInt(255),
-                                  Random().nextInt(255)))),
-                      //第1个片段，黑色样式
-                      TextSpan(
-                          text: 'View 3 ',
-                          style: TextStyle(
-                              color: Color.fromARGB(
-                                  255,
-                                  Random().nextInt(255),
-                                  Random().nextInt(255),
-                                  Random().nextInt(255)))),
-                      //第1个片段，红色样式
-                      TextSpan(
-                          text: 'Color Text',
-                          style: TextStyle(
-                              color: Color.fromARGB(
-                                  255,
-                                  Random().nextInt(255),
-                                  Random().nextInt(255),
-                                  Random().nextInt(255))))
-                      //第1个片段，黑色样式
-                    ]),
-                    textAlign: TextAlign.center,
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.all(5),
+                    child: assetsImg = Image.asset(
+                      "assets/images/doge.jpg",
+                      width: MediaQuery.of(context).size.width - 10,
+                      fit: BoxFit.fitWidth,
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.all(5),
+                    child: Image.network(
+                      "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png",
+                      width: MediaQuery.of(context).size.width - 10,
+                      fit: BoxFit.fitWidth,
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.all(5),
+                    child: RaisedButton(
+                        child: Text("Raised button"),
+                        onPressed: () {
+                          print("raised button on press");
+                        },
+                        textColor: Color.fromARGB(255, Random().nextInt(255),
+                            Random().nextInt(255), Random().nextInt(255)),
+                        color: Color.fromARGB(255, Random().nextInt(255),
+                            Random().nextInt(255), Random().nextInt(255))),
+                  )
+                ],
+              ),
+              Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.all(5),
+                        child: FlatButton(
+                            child: Text("Flat button"),
+                            onPressed: handleFlatButtonPressed,
+                            textColor: Color.fromARGB(255, Random().nextInt(255),
+                                Random().nextInt(255), Random().nextInt(255)),
+                            color: Color.fromARGB(255, Random().nextInt(255),
+                                Random().nextInt(255), Random().nextInt(255))),
+                      )
+                    ],
                   ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.all(5),
-                  child: Image.asset(
-                 "assets/images/doge.jpg",
-                    width: MediaQuery.of(context).size.width-10,
-                    fit: BoxFit.fitWidth,
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.all(5),
+                        child: DropdownButton<String>(
+                            value:dropdownValue,
+                          items: <String>[
+                            "drop item1","drop item 2",
+                            "drop item 3","drop item 4",
+                            "drop item 5"].
+                          map<DropdownMenuItem<String>>((String value)
+                          {return DropdownMenuItem<String>(value: value, child: Text(value),);}).toList(),
+                          icon: Icon(Icons.arrow_downward),
+                          iconSize: 24,
+                          elevation: 16,
+                          style: TextStyle(
+                              color: Colors.deepPurple
+                          ),
+                          underline: Container(
+                            height: 2,
+                            color: Colors.deepPurpleAccent,
+                          ),
+                          onChanged: (String newValue) {
+                            setState(() {
+                              dropdownValue = newValue;
+                            });
+                          },),
+                      )
+                    ],
                   ),
-                )
-              ],
-            ),
-          ]),
-    );
+            ])));
   }
 
   @override
   void dispose() {
     super.dispose();
+  }
+
+  handleFlatButtonPressed() {
+    print("flatButton pressed");
   }
 }
